83498fd78d9d113f5372093e08b4843844005d2e
David Kiki <david.kariuki2@student.moringaschool.com>
2025-05-09 09:24:26 +0300

    Completed work

--- lib/a_type_error.py
+++ lib/a_type_error.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
-wrong_type = 'abc' + 123
+wrong_type = 'abc' + str(123)
+print(wrong_type)
+# This code will raise a TypeError because we are trying to add a string and an integer.
